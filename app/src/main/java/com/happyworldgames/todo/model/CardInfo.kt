cdafc6451b4bd2145db07d16c2acc05eecf3e052
HappyWorld <xeler.arsk@gmail.com>
2022-06-17 21:29:14 +0300

    cardInfo add comment & tagList, checkList

--- app/src/main/java/com/happyworldgames/todo/model/CardInfo.kt
+++ app/src/main/java/com/happyworldgames/todo/model/CardInfo.kt
@@ -4,8 +4,15 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CardInfo(
-    override var id: String,
-    override var position: Int,
-    override var name: String,
-    var description: String         // card description
+    override var id: String,                // card folder name
+    override var position: Int,             // card position
+    override var name: String,              // card name
+    var description: String,                // card description
+    val tagList: ArrayList<TagItem>,        // tag list
+    val checkList: ArrayList<CheckItem>     // task check list
     ): InfoInterface
+
+@Serializable
+data class TagItem(var name: String, var color: Int)
+@Serializable
+data class CheckItem(var name: String, var boolean: Boolean = false)
